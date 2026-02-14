678fdb19bd5d4f3ae225dea800d82e708df5dec1
hovhannisyandavid <davidhovhannisyantgn@gmail.com>
2026-02-14 05:13:55 +0100

    Update hardening.sh

--- scripts/hardening.sh
+++ scripts/hardening.sh
@@ -8,5 +8,10 @@ sudo ufw allow 80/tcp
 sudo ufw allow 3306/tcp
 sudo ufw --force enable
 
-sudo sed -i 's/PermitRootLogin yes/PermitRootLogin no/' /etc/ssh/sshd_config || true
+# SSH hardening 
+sudo sed -i 's/PermitRootLogin yes/PermitRootLogin no/' /etc/ssh/sshd_config || true 
+sudo sed -i 's/#PasswordAuthentication yes/PasswordAuthentication no/' /etc/ssh/sshd_config || true 
+sudo sed -i 's/#PermitEmptyPasswords no/PermitEmptyPasswords no/' /etc/ssh/sshd_config || true 
+sudo sed -i 's/#MaxAuthTries 6/MaxAuthTries 3/' /etc/ssh/sshd_config || true 
+sudo sed -i 's/#X11Forwarding yes/X11Forwarding no/' /etc/ssh/sshd_config || true
 sudo systemctl restart ssh || true
